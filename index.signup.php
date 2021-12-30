000079bfc2e6cb7b78e242b50b661ba2d4002962
Thebks <ichbks@gmail.com>
2021-12-30 11:17:11 +0500

    WIP signup

---
+++ index.signup.php
@@ -0,0 +1,52 @@
+<?php 
+
+session_start();
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
+<body>
+	<section class="index-login">
+	  <div class="d-flex justify-content-center align-items-center" style="min-height: 100vh;">
+	  <div class="index-login-login">	
+	  	<form class="p-5 rounded shadow" action="login.inc.php" method="post" style="width: 30rem">
+	  		<h1 class="text-center pb-5 display-4">Signup</h1>
+          <div class="mb-3">
+		    <label for="FirstName" class="form-label">First Name</label>
+		    <input type="text" name="first_name" placeholder="First name" class="form-control">
+		  </div>
+          <div class="mb-3">
+		    <label for="LastName" class="form-label">Last name</label>
+		    <input type="text" name="last_name" placeholder="Last name" class="form-control">
+		  </div>
+		  <div class="mb-3">
+		    <label for="exampleInputEmail1" class="form-label">Email Address</label>
+		    <input type="email" name="email" placeholder="Email" class="form-control">
+		  </div>
+
+		  <div class="mb-3">
+		    <label for="exampleInputPassword1" class="form-label">Password</label>
+		    <input type="password" class="form-control" name="pass" placeholder="Password">
+		  </div>
+
+          <div class="mb-3">
+		    <label for="exampleInputPassword2" class="form-label">Repeat Password</label>
+		    <input type="password" name="pass" placeholder="Repeat password" class="form-control">
+		  </div>
+		  <button type="submit" class="btn btn-primary">LOGIN</button>
+		
+		</form>
+	  </div>
+	  </div>
+	</section>
+</body>
+</html>
+
--- init.php
+++
@@ -1,9 +0,0 @@
-<?php
-  //  define("DB_HOST", "172.18.0.2");
-   // define("DB_NAME", "loginproject");
-   // define("DB_USER", "root");
-   // define("DB_PASS", "rootadmin");
-   // define("BASE_URL", "http://localhost:8090/");
-   // include("Database.php");
-
-
